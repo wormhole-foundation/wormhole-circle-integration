45c11abf85a9d22c6bfae19f5a9365b50d66425a
gator-boi <gator-boi@users.noreply.github.com>
2022-10-14 16:33:50 +0000

    Add depositForBurn call

--- evm/forge-test/CrossChainUSDC.t.sol
+++ evm/forge-test/CrossChainUSDC.t.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/interfaces/IWormhole.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {WormholeSimulator} from "wormhole-solidity/WormholeSimulator.sol";
 
 import "forge-std/console.sol";
@@ -14,15 +16,26 @@ interface IUSDC {
     function masterMinter() external view returns (address);
 }
 
+interface IUSDCBridge {
+    function depositForBurnWithCaller(
+        uint256 _amount,
+        uint32 _destinationDomain,
+        bytes32 _mintRecipient,
+        address _burnToken,
+        bytes32 _destinationCaller
+    ) external returns (uint64 _nonce);
+}
+
 contract CrossChainUSDC is Test {
     IWormhole wormhole;
     uint256 guardianSigner;
 
-    // wormhole simulator
+    // Wormhole simulator
     WormholeSimulator public wormholeSimulator;
 
-    // usdc
+    // USDC
     IUSDC usdc = IUSDC(0x07865c6E87B9F70255377e024ace6630C1Eaa37F);
+    IUSDCBridge usdcBridge = IUSDCBridge(0xdAbec94B97F7b5FCA28f050cC8EeAc2Dc9920476);
 
     function setUp() public {
         // verify that we're using the correct fork (AVAX mainnet in this case)
@@ -60,4 +73,26 @@ contract CrossChainUSDC is Test {
         assertEq(balance, 1000e6);
     }
 
+    function testDeposit() public {
+        // variables needed to call the USDC Bridge contract
+        uint32 destinationDomain = 1;
+        bytes32 mintRecipient = bytes32(uint256(uint160(address(this))));
+        uint256 amount = 1e6;
+        bytes32 targetWormholeContract = bytes32(uint256(uint160(address(this))));
+
+        SafeERC20.safeApprove(
+            IERC20(address(usdc)),
+            address(usdcBridge),
+            amount
+        );
+
+        // burn USDC on the bridge
+        uint64 nonce = usdcBridge.depositForBurnWithCaller(
+            amount,
+            destinationDomain,
+            mintRecipient,
+            address(usdc),
+            targetWormholeContract
+        );
+    }
 }
