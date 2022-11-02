8d0aac3896f9758c238e146f319782e459bc7a13
Reptile <43194093+gator-boi@users.noreply.github.com>
2022-11-02 15:00:13 -0500

    Evm/interface (#2)
    * Add EVM interface
    
    * Update README.md
    
    Co-authored-by: gator-boi <gator-boi@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,12 +1,8 @@
 # wormhole-circle-integration
 
-### Disclaimer
-
-This is an example repo and it should not be deployed to mainnet without additional testing and security auditing.
-
 ### Details
 
-This repo demonstrates how to send USDC cross-chain (with or without additional instructions to be used by a contract) by using Wormhole's generic-messaging layer and by interacting with Circle's Cross-Chain Transfer Protocol.
+This repo demonstrates how to send USDC cross-chain (with additional instructions to be used by a contract) by using Wormhole's generic-messaging layer and by interacting with Circle's Cross-Chain Transfer Protocol.
 
 ### Compiling contracts
 
@@ -23,10 +19,10 @@ make build
 cd evm
 
 # goerli
-. env/eth-goerli-testnet.env && PRIVATE_KEY=put_your_private_key_here bash shell-scripts/deploy_usdc_integration.sh
+. env/eth-goerli-testnet.env && PRIVATE_KEY=put_your_private_key_here bash shell-scripts/deploy_circle_integration.sh
 
 # fuji
-. env/avax-fuji-testnet.env && PRIVATE_KEY=put_your_private_key_here bash shell-scripts/deploy_usdc_integration.sh
+. env/avax-fuji-testnet.env && PRIVATE_KEY=put_your_private_key_here bash shell-scripts/deploy_circle_integration.sh
 ```
 
 ### Deployed Contract Addresses
@@ -45,18 +41,13 @@ struct RedeemParameters {
     bytes circleAttestation;
 }
 
-struct WormholeDeposit {
-    uint8 payloadId;
+struct WormholeDepositWithPayload {
+    uint8 payloadId; // == 1
     bytes32 token;
     uint256 amount;
     uint32 sourceDomain;
     uint32 targetDomain;
     uint64 nonce;
-    bytes32 circleSender; // circle bridge on this chain
-}
-
-struct WormholeDepositWithPayload {
-    WormholeDeposit depositHeader;
     bytes32 mintRecipient;
     bytes payload;
 }
@@ -78,13 +69,6 @@ struct CircleDeposit {
 ### API
 
 ```solidity
-function transferTokens(
-    address token,
-    uint256 amount,
-    uint16 targetChain,
-    bytes32 mintRecipient
-) public payable returns (uint64 messageSequence)
-
 function transferTokensWithPayload(
     address token,
     uint256 amount,
@@ -92,8 +76,6 @@ function transferTokensWithPayload(
     bytes32 mintRecipient,
     bytes memory payload
 ) public payable returns (uint64 messageSequence)
-
-function redeemTokens(RedeemParameters memory params) public
 
 function redeemTokensWithPayload(
     RedeemParameters memory params
--- evm/src/interfaces/ICircleIntegration.sol
+++ evm/src/interfaces/ICircleIntegration.sol
@@ -2,7 +2,86 @@
 
 pragma solidity ^0.8.13;
 
+import {IWormhole} from "./IWormhole.sol";
+import {ICircleBridge} from "./circle/ICircleBridge.sol";
+import {IMessageTransmitter} from "./circle/IMessageTransmitter.sol";
+
 interface ICircleIntegration {
+    struct RedeemParameters {
+        bytes encodedWormholeMessage;
+        bytes circleBridgeMessage;
+        bytes circleAttestation;
+    }
+
+    struct WormholeDepositWithPayload {
+        uint8 payloadId; // == 1
+        bytes32 token;
+        uint256 amount;
+        uint32 sourceDomain;
+        uint32 targetDomain;
+        uint64 nonce;
+        bytes32 mintRecipient;
+        bytes payload;
+    }
+
+    struct CircleDeposit {
+        // Message Header
+        uint32 version;
+        uint32 sourceDomain;
+        uint32 targetDomain;
+        uint64 nonce;
+        bytes32 circleSender;
+        bytes32 circleReceiver;
+        // End of Message Header
+        // There should be an arbitrary length message following the header,
+        // but we don't need to parse this message for verification purposes.
+    }
+
+    function transferTokensWithPayload(
+        address token,
+        uint256 amount,
+        uint16 targetChain,
+        bytes32 mintRecipient,
+        bytes memory payload
+    ) external payable returns (uint64 messageSequence);
+
+    function redeemTokensWithPayload(
+        RedeemParameters memory params
+    ) external returns (WormholeDepositWithPayload memory wormholeDepositWithPayload);
+
+    function encodeWormholeDepositWithPayload(
+        WormholeDepositWithPayload memory message
+    ) external pure returns (bytes memory);
+
+    function decodeWormholeDepositWithPayload(
+        bytes memory encoded
+    ) external pure returns (WormholeDepositWithPayload memory message);
+
+    function decodeCircleDeposit(
+        bytes memory encoded
+    ) external pure returns (CircleDeposit memory message);
+
     function owner() external view returns (address);
 
+    function pendingOwner() external view returns (address);
+
+    function isInitialized(address impl) external view returns (bool);
+
+    function wormhole() external view returns (IWormhole);
+
+    function chainId() external view returns (uint16);
+
+    function wormholeFinality() external view returns (uint8);
+
+    function circleBridge() external view returns (ICircleBridge);
+
+    function circleTransmitter() external view returns (IMessageTransmitter);
+
+    function getRegisteredEmitter(uint16 emitterChainId) external view returns (bytes32);
+
+    function isAcceptedToken(address token) external view returns (bool);
+
+    function getChainDomain(uint16 chainId_) external view returns (uint32);
+
+    function isMessageConsumed(bytes32 hash) external view returns (bool);
 }
