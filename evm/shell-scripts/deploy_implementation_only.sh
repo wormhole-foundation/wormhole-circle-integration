c6791ccf5b9f8766a9f0498338d0c9f2c7978899
Sebastián Claudio Nale <sebinale@gmail.com>
2023-09-27 19:05:03 -0300

    evm: fixes env var for RPC URLs

--- evm/shell-scripts/deploy_implementation_only.sh
+++ evm/shell-scripts/deploy_implementation_only.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 forge script $(dirname $0)/../forge-scripts/deploy_implementation_only.sol \
     -vv \
-    --rpc-url $RPC \
+    --rpc-url $CONFIGURE_CCTP_RPC \
     --broadcast --slow $@
