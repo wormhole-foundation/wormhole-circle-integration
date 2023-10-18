04fb6681533b65f4ab123b605af5e387ed3533d8
solanoe <solanoepalacio@gmail.com>
2023-10-18 19:01:04 -0300

    adds base deployment configuration

--- evm/shell-scripts/submit_testnet_registration.sh
+++ evm/shell-scripts/submit_testnet_registration.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
-"""
-Usage: ./submit_testnet_registration <target chain> <foreign chain> <foreign emitter> <foreign domain> <forge script args (keys)>
-"""
+
+# Usage: ./submit_testnet_registration <target chain> <foreign chain> <foreign emitter> <foreign domain> <forge script args (keys)>
+
 set -euo pipefail
 
 export TARGET_CHAIN=$1
 export FOREIGN_CHAIN=$2
 export FOREIGN_EMITTER=$3
 export FOREIGN_DOMAIN=$4
+export SIGNER_KEY=$5
 
-slice 4 # <- remove 4 first arguments
+shift 5 # <- remove 5 first arguments
+
 forge script $(dirname $0)/../forge-scripts/submit_testnet_registration.sol \
     -vv \
-    --rpc-url $RPC \
+    --rpc-url $CONFIGURE_CCTP_RPC \
     --broadcast --slow $@
