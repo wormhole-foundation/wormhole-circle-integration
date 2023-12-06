940bc42b4d936e2eb63032de2c68909081e2aa97
solanoe <solanoepalacio@gmail.com>
2023-12-06 18:01:33 -0300

    minor fix on verify_proxy script

--- evm/shell-scripts/verify_proxy.sh
+++ evm/shell-scripts/verify_proxy.sh
@@ -5,8 +5,8 @@ etherscan_key=$1
 governance_chain_id=1
 governance_contract=0x0000000000000000000000000000000000000000000000000000000000000004
 
-setup_bytes=$(cast calldata "function setup(address,address,uint8,address,uint16,bytes32)" $CIRCLE_INTEGRATION_IMPLEMENTATION $RELEASE_WORMHOLE_ADDRESS $RELEASE_WORMHOLE_FINALITY $RELEASE_CIRCLE_BRIDGE_ADDRESS $governance_chain_id $governance_contract)
-
+setup_bytes=$(cast calldata "setup(address,address,uint8,address,uint16,bytes32)" $CIRCLE_INTEGRATION_IMPLEMENTATION $RELEASE_WORMHOLE_ADDRESS $RELEASE_WORMHOLE_FINALITY $RELEASE_CIRCLE_BRIDGE_ADDRESS $governance_chain_id $governance_contract)
+echo "setup bytes: $setup_bytes";
 forge verify-contract --chain-id $RELEASE_EVM_CHAIN_ID --watch \
     --constructor-args $(cast abi-encode "constructor(address,bytes)" $CIRCLE_INTEGRATION_SETUP $setup_bytes) \
     --compiler-version v0.8.19 $CIRCLE_INTEGRATION_PROXY \
